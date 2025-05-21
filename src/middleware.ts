3ed1b12107e8f511e39d9a4e301841bdb23a5abe
Ahsan Khan <ahsan.ak943@gmail.com>
2025-05-21 22:04:30 +0500

    test: base fun imple

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,23 +4,23 @@ let locales = ["en", "de"];
 const defaultLocale = "en";
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-  const pathnameHasLocale = locales.some(
-    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-  );
+  // const { pathname } = request.nextUrl;
+  // const pathnameHasLocale = locales.some(
+  //   (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  // );
 
-  if (pathnameHasLocale) return NextResponse.next();
+  // if (pathnameHasLocale) return NextResponse.next();
 
-  request.nextUrl.pathname = `/${defaultLocale}${pathname}`;
-  const response = NextResponse.redirect(request.nextUrl);
-  response.cookies.set("NEXT_LOCALE", defaultLocale, {
-    path: "/",
-    httpOnly: true,
-    sameSite: "lax",
-    secure: process.env.NODE_ENV === "production",
-    maxAge: 60 * 60 * 24 * 365,
-  });
-  return response;
+  // request.nextUrl.pathname = `/${defaultLocale}${pathname}`;
+  // const response = NextResponse.redirect(request.nextUrl);
+  // response.cookies.set("NEXT_LOCALE", defaultLocale, {
+  //   path: "/",
+  //   httpOnly: true,
+  //   sameSite: "lax",
+  //   secure: process.env.NODE_ENV === "production",
+  //   maxAge: 60 * 60 * 24 * 365,
+  // });
+  // return response;
 }
 
 export const config = {
